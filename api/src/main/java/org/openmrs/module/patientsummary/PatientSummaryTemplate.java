dc3a973fa0776f7959b3c5157e94652eec005277
mseaton <mseaton@pih.org>
2015-12-13 03:01:39 -0500

    default all patient summaries to html for now

--- api/src/main/java/org/openmrs/module/patientsummary/PatientSummaryTemplate.java
+++ api/src/main/java/org/openmrs/module/patientsummary/PatientSummaryTemplate.java
@@ -13,116 +13,93 @@
  */
 package org.openmrs.module.patientsummary;
 
-import java.io.IOException;
-import java.io.OutputStream;
-
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.module.reporting.report.ReportData;
 import org.openmrs.module.reporting.report.ReportDesign;
-import org.openmrs.module.reporting.report.renderer.RenderingException;
-import org.openmrs.module.reporting.report.renderer.ReportTemplateRenderer;
 
 /**
  * Represents a particular Patient Summary Template
  */
-public class PatientSummaryTemplate extends ReportTemplateRenderer {
-	
+public class PatientSummaryTemplate {
+
 	protected final Log log = LogFactory.getLog(this.getClass());
-	
-	// ***** PROPERTIES *****
-	
+
+	//***** PROPERTIES *****
+
 	private ReportDesign reportDesign;
-	
-	// ***** CONSTRUCTORS *****
-	
+
+	//***** CONSTRUCTORS *****
+
 	/**
 	 * Default constructor
 	 */
-	public PatientSummaryTemplate() {
-	}
-	
+	public PatientSummaryTemplate() {}
+
 	/**
 	 * Full Constructor
 	 */
 	public PatientSummaryTemplate(ReportDesign reportDesign) {
 		this.reportDesign = reportDesign;
 	}
-	
-	// ***** METHODS *****
-	
+
+	//***** METHODS *****
+
 	/**
 	 * @return the contentType for this PatientSummary
 	 */
 	public String getContentType() {
-		try {
-			String contentType = getTemplate(reportDesign).getContentType();
-			if (StringUtils.isNotEmpty(contentType)) {
-				return contentType;
-			}
-		}
-		catch (Exception e) {
-			log.warn("Unable to retrieve content type for patient summary: " + reportDesign);
-		}
-		return "text/html";
+        return "text/html";
 	}
-	
+
 	/**
 	 * @return the filename for this PatientSummary for use when exporting
 	 */
 	public String getExportFilename() {
-		try {
-			return getExportFilename();
-		}
-		catch (Exception e) {
-			log.warn("Unable to retrieve file name for patient summary: " + reportDesign);
-		}
-		return "summary.txt";
+        return reportDesign.getName() + ".html";
 	}
-	
+
 	/**
 	 * @return the primary key id
 	 */
 	public Integer getId() {
 		return getReportDesign() != null ? getReportDesign().getId() : null;
 	}
-	
+
 	/**
 	 * @return the uuid
 	 */
 	public String getUuid() {
 		return getReportDesign() != null ? getReportDesign().getUuid() : null;
 	}
-	
+
 	/**
 	 * @return the name
 	 */
 	public String getName() {
 		return getReportDesign() != null ? getReportDesign().getName() : null;
 	}
-	
+
 	/**
 	 * @return the associated patient summary report definition
 	 */
 	public PatientSummaryReportDefinition getReportDefinition() {
-		return getReportDesign() != null ? (PatientSummaryReportDefinition) getReportDesign().getReportDefinition() : null;
+		return getReportDesign() != null ? (PatientSummaryReportDefinition)getReportDesign().getReportDefinition() : null;
 	}
-	
+
 	/**
 	 * @see Object#equals(Object)
 	 */
 	@Override
 	public boolean equals(Object o) {
 		if (o != null && o instanceof PatientSummaryTemplate) {
-			PatientSummaryTemplate that = (PatientSummaryTemplate) o;
+			PatientSummaryTemplate that = (PatientSummaryTemplate)o;
 			if (this.getReportDesign() != null && that.getReportDesign() != null) {
 				return this.getReportDesign().equals(that.getReportDesign());
 			}
 		}
 		return super.equals(o);
 	}
-	
+
 	/**
 	 * @see Object#hashCode()
 	 */
@@ -133,7 +110,7 @@ public class PatientSummaryTemplate extends ReportTemplateRenderer {
 		}
 		return super.hashCode();
 	}
-	
+
 	/**
 	 * @see Object#toString()
 	 */
@@ -147,24 +124,20 @@ public class PatientSummaryTemplate extends ReportTemplateRenderer {
 		}
 		return super.toString();
 	}
-	
-	// ***** PROPERTY ACCESS *****
-	
+
+	//***** PROPERTY ACCESS *****
+
 	/**
 	 * @return the reportDesign
 	 */
 	public ReportDesign getReportDesign() {
 		return reportDesign;
 	}
-	
+
 	/**
 	 * @param reportDesign the reportDesign to set
 	 */
 	public void setReportDesign(ReportDesign reportDesign) {
 		this.reportDesign = reportDesign;
-	}
-	
-	@Override
-	public void render(ReportData arg0, String arg1, OutputStream arg2) throws IOException, RenderingException {
 	}
 }
